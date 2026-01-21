aa26aeb767ccc5fafb04f7a08e75f452042797b8
sarthakGup05 <rythmgupta@outlook.com>
2026-01-21 12:20:46 +0530

    feat: add ImageKit authentication API endpoint.

--- app/api/imagekit/auth/route.ts
+++ app/api/imagekit/auth/route.ts
@@ -1,6 +1,8 @@
 import ImageKit from "imagekit";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 const imagekit = new ImageKit({
   publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
